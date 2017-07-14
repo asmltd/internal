f5e761167a4b0430302a0ad61e8723877eb2496e
raagavendran <raagavendran@gmail.com>
2017-07-14 16:05:21 +0530

    Added Delete_user Button and function

--- RestProjDjango/media/modules/add_user/add_user.js
+++ RestProjDjango/media/modules/add_user/add_user.js
@@ -1,37 +1,24 @@
 app.controller('add_user_controller', function($rootScope, $scope, $state, $http, $interval) {
 
-            //    $scope.addUser = function(){
-            //            console.log($scope.user)}
+    $scope.user = {};
+    // calling our submit function.
+    $scope.addUser = function() {
+        // Posting data to php file
+        $http({
+                method: 'POST',
+                url: '/api/Sessions/',
+                data: $scope.user, //forms user object
+            })
+            .success(function(data) {
+                if (data.errors) {
+                    // Showing errors.
+                    $scope.errorName = data.errors.name;
+                    $scope.errorUserName = data.errors.username;
+                    $scope.errorEmail = data.errors.email;
+                } else {
+                    $scope.message = data.message;
+                }
+            });
 
-
-            //$scope.Details = [];
-            //$http.get('/api/Sessions/', '')
-            //            .success(function (data) {
-            //                $scope.Details = data;
-            //            })
-
-
-            $scope.user = {};
-            // calling our submit function.
-            $scope.addUser = function() {
-                // Posting data to php file
-                $http({
-                        method: 'POST',
-                        url: '/api/Sessions/',
-                        data: $scope.user, //forms user object
-//                        headers: {
-//                            'Content-Type': 'application/x-www-form-urlencoded'
-//                        }
-                    })
-                    .success(function(data) {
-                        if (data.errors) {
-                            // Showing errors.
-                            $scope.errorName = data.errors.name;
-                            $scope.errorUserName = data.errors.username;
-                            $scope.errorEmail = data.errors.email;
-                        } else {
-                            $scope.message = data.message;
-                        }
-                    });
-
-            }});
+    }
+});
--- RestProjDjango/media/modules/user_details/user_details.js
+++ RestProjDjango/media/modules/user_details/user_details.js
@@ -1,8 +1,22 @@
-app.controller('user_details', function ($rootScope, $scope, $state,$http, $interval) {
-$scope.Details = [];
-$http.get('/api/Sessions/', '')
-            .success(function (data) {
+app.controller('user_details', function($rootScope, $scope, $state, $http, $interval) {
+    $scope.Details = [];
+
+    $scope.loadUser = function() {
+        $http.get('/api/Sessions/', '')
+            .success(function(data) {
                 $scope.Details = data;
             })
-});
+    }
 
+    $scope.loadUser();
+
+    $scope.del_user = function(id) {
+        $http({
+                method: 'DELETE',
+                url: '/api/Sessions/' + id + "/",
+            })
+            .success(function(data) {
+                $scope.loadUser();
+            });
+    }
+});
